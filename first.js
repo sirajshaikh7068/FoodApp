bb4ab7153956ee095bec01660fc4fc6b7cb05808
sirajshaikh7068 <sirajshaikh.skn.entc@gmail.com>
2024-01-09 14:25:29 +0530

    episode-03

--- first.js
+++ first.js
@@ -1,24 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
+//React Element
+const heading = (
+<h1 id ="heading1">hello i am react Element</h1>)
 
-const parent = React.createElement("div",{ id : "parent"},
-   [ React.createElement("div",{ id : "child"},
-     
-   [React.createElement("h1",{},"i am h1"),
-     React.createElement("h2",{},"i am h2")] 
-),
-React.createElement("div",{ id : "child"},
-     
-[React.createElement("h1",{},"i am h1"),
-  React.createElement("h2",{},"i am h2")]
 
-)]
-
+//functional component
+const Jsx= () => (
+   <h1 id ="heading">This is jsx syntax</h1>
 )
 
+const HeadingComponent= () => (
+  <div id="container">
+    {heading}                          {/* adding Elementin functional component */}
+      <Jsx/>                            {/*adding functional component in functional component*/}
+        <Jsx></Jsx>                     {/*adding functional component in functional component*/}
+          {Jsx()}                      { /*adding functional component in functional component*/}
+    <h1 id ="heading">This is functional component</h1>
+  </div>)
 
 
  const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent/>);
